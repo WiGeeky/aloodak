4e2496516a1c85f1095de34143a5f695d197b871
Mohammad H. Maazallahi <mohammadhoseinmazalahi@gmail.com>
2021-01-11 15:49:34 +0330

    Reversing api path change mistake

--- main.py
+++ main.py
@@ -39,5 +39,5 @@ if __name__ == '__main__':
     if info.aqi != tracker.last_measure:
         info.generateImage()
         info.generateCaption()
-        post(f'https://mikey.ir/bot{config.telegram_api_key}/sendPhoto', data={'chat_id': config.telegram_chat_id, 'caption': info.caption, 'disable_notifications': True}, files={'photo': info.imageb})
+        post(f'https://api.telegram.org/bot{config.telegram_api_key}/sendPhoto', data={'chat_id': config.telegram_chat_id, 'caption': info.caption, 'disable_notifications': True}, files={'photo': info.imageb})
         tracker.setMeasure(info.aqi)
